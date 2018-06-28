9e9b2ba5ce28bf84a5683fa8e14aa58030a902f2
Qasmi2 <nadeemqasmi40@yahoo.com>
2018-06-28 05:11:15 -0700

    updated file sturcture

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include 'header.php';?>
+
+    <div class="container">
+    <h1 class="text-center">hello world</h1>
+        
+    <div class="col-md-12">
+        <div class="row">
+            <div clas="col-md-4" style="margin:30px;">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="http://www.georgehart.com/cards/cards-60.jpg" height="300" width="300" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Card </h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Button</a>
+                    </div>
+                </div>
+            </div>
+            <div clas="col-md-4" style="margin:30px;">
+            <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="http://www.georgehart.com/cards/cards-3.jpg" height="300" width="300" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card </h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Button</a>
+                    </div>
+            </div>
+            </div>
+            <div clas="col-md-4" style="margin:30px;">
+                <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="images/te.png" height="300" width="300" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">Card </h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+        </div>              
+    </div>
+    <div class="container">
+    <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+<?php include 'footer.php';?>
